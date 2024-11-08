03e64b5e8467c2797333bfeac90d9c49e5028094
takehiro177 <takehiro177>
2024-11-08 08:20:12 +0000

    add preprocessing

---
+++ src/data_preprocessing/bronze/bronze_sales_table.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+# Retrieve the secret from the secret scope to access data lake storage container
+storage_account_key = dbutils.secrets.get(scope="tk-personal", key="substackposts-storage-account-key")
+spark.conf.set("fs.azure.account.key.takehiropersonal.dfs.core.windows.net", storage_account_key)
+
+# COMMAND ----------
+
+# Configure Auto Loader to read from your Azure Blob Storage Gen2 with folder structure
+query = (spark.readStream
+              .format("cloudFiles")
+              .option("cloudFiles.format", "csv")
+              .option("header", "true") 
+              .option("pathGlobfilter", "*.csv")
+              .option("cloudFiles.schemaLocation", "dbfs:/end_to_end_demand_forecast/checkpoint/bronze/sales")
+              .load("abfss://sales-datasource@takehiropersonal.dfs.core.windows.net/sales_hist/")
+            .writeStream
+              .format("delta")
+              .outputMode("append")
+              .option("checkpointLocation", "dbfs:/end_to_end_demand_forecast/checkpoint/bronze/sales")
+              .trigger(availableNow=True)
+              .table("portfolio.end_to_end_demand_forecast.bronze_sales_table")
+)
+
+query.awaitTermination()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM portfolio.end_to_end_demand_forecast.bronze_sales_table
+
+# COMMAND ----------
+
+#%sql
+##DROP TABLE portfolio.end_to_end_demand_forecast.bronze_sales_table
+
+# COMMAND ----------
+
+##dbutils.fs.rm("dbfs:/end_to_end_demand_forecast/checkpoint/bronze/sales", True)
