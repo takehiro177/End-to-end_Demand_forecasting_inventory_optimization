5326ae19d76d544362470f1a72e1766b048cf94b
takehiro177 <takehiro177>
2024-11-19 12:01:14 +0000

    add image embedding gold table

---
+++ src/data_preprocessing/gold/gold_imgembed_table.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+# MAGIC %pip install imgbeddings
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# Library
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from pyspark.sql.functions import udf, pandas_udf, PandasUDFType
+from pyspark.sql.types import ArrayType, StringType, IntegerType, FloatType
+
+import io
+import numpy as np
+import pandas as pd
+
+from PIL import Image, ImageOps
+
+from imgbeddings import imgbeddings
+
+# COMMAND ----------
+
+def extract_img_embed(content):
+  """Extract image embedding from its raw content."""
+  image = Image.open(io.BytesIO(content))
+  img_embed = imgbeddings()
+  embed = img_embed.to_embeddings(image)
+
+  return embed[0].tolist()
+
+@pandas_udf(ArrayType(FloatType()))
+def extract_img_embed_udf(content_series):
+  """Extract image embedding from its raw content."""
+  embeds = content_series.apply(lambda content: extract_img_embed(content))
+
+  return embeds
+
+# COMMAND ----------
+
+preprocessed_clothes_table = (spark.read.table("portfolio.end_to_end_demand_forecast.silver_clothes_table")
+                        .select(F.col("content"),
+                                F.col("category"),
+                                F.col("file_name"),
+                                )
+                        .withColumn("img_path", F.concat(F.col("category"), F.lit("/"), F.col("file_name")))
+                        .withColumn("embed", extract_img_embed_udf(F.col("content")))
+                )
+        
+
+
+# COMMAND ----------
+
+preprocessed_clothes_table.write.format("delta").mode("overwrite").option("overwriteSchema", "True").saveAsTable("portfolio.end_to_end_demand_forecast.gold_imgembed_table")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM portfolio.end_to_end_demand_forecast.gold_imgembed_table
+
+# COMMAND ----------
+
+#%sql
+##DROP TABLE portfolio.end_to_end_demand_forecast.gold_imgembed_table
--- src/data_preprocessing/silver/silver_clothes_table.py
+++ src/data_preprocessing/silver/silver_clothes_table.py
@@ -1,12 +1,6 @@
 # Databricks notebook source
-# Retrieve the secret from the secret scope
-storage_account_key = dbutils.secrets.get(scope="tk-personal", key="substackposts-storage-account-key")
-spark.conf.set("fs.azure.account.key.takehiropersonal.dfs.core.windows.net", storage_account_key)
-
-# COMMAND ----------
-
 # Install Library
-%pip install fastembed-gpu
+%pip install imgbeddings
 
 # COMMAND ----------
 
@@ -26,7 +20,7 @@ import pandas as pd
 from pyspark.sql.functions import pandas_udf
 from PIL import Image, ImageOps
 
-from fastembed import ImageEmbedding
+from imgbeddings import imgbeddings
 
 # COMMAND ----------
 
@@ -45,22 +39,6 @@ def extract_size_udf(content_series):
   sizes = content_series.apply(extract_size)
   return pd.DataFrame(list(sizes))
 
-def extract_img_embed(content):
-  """Extract image embedding from its raw content."""
-  image = Image.open(io.BytesIO(content))
-  image = ImageOps.fit(image, (128, 128), Image.LANCZOS)
-  model = ImageEmbedding(model_name="Qdrant/clip-ViT-B-32-vision", cuda=True)
-  embed = model.embed(image)
-
-  return embed
-
-@pandas_udf("array<decimal>")
-def extract_img_embed_udf(content_series):
-  """Extract image embedding from its raw content."""
-  embeds= content_series.apply(extract_img_embed)
-
-  return pd.DataFrame(list(embeds))
-
 # COMMAND ----------
 
 def batch_upsert(microBatchDF, batchId):
@@ -69,9 +47,8 @@ def batch_upsert(microBatchDF, batchId):
                     .withColumn("category", F.split(extract_category("path"), '/').getItem(0))
                     .withColumn("file_name", F.split(extract_category("path"), '/').getItem(1))
                     .withColumn("size", extract_size_udf(F.col("content")))
-                    #.withColumn("embed", extract_img_embed_udf("content"))
                  )
-
+    
     (microBatchDF.write.format("delta")
                  .mode("append")
                  .option("mergeSchema", "true")
@@ -80,7 +57,7 @@ def batch_upsert(microBatchDF, batchId):
 
 # COMMAND ----------
 
-query = (spark.readStream.option("maxBytesPerTrigger", "1g").table("portfolio.end_to_end_demand_forecast.bronze_clothes_table")
+query = (spark.readStream.table("portfolio.end_to_end_demand_forecast.bronze_clothes_table")
                         .writeStream
                          .foreachBatch(batch_upsert)
                          .option("checkpointLocation", "dbfs:/end_to_end_demand_forecast/checkpoint/silver/clothes_retail_image")
